c01f0f151d3eff4e07d2392f4c249b1efcb4890e
christianc2000 <christiancelsomamanisoliz0@gmail.com>
2022-06-22 15:15:47 -0400

    login

--- app/Models/User.php
+++ app/Models/User.php
@@ -51,50 +51,5 @@ class User extends Authenticatable
         return $this->hasOne(Conductor::class);
     }
     //************************************************* */
-    public function scopeIncluded(Builder $query)
-    {
-        if (empty($this->allowIncluded) || empty(request('included'))) {
-            return;
-        }
-        $relations = explode(',', request('included')); //explode conveierte un string con ',' en array['transportes','relacion'] 
-
-        $allowIncluded = collect($this->allowIncluded);
-        foreach ($relations as $key => $relationsShip) {
-            if (!$allowIncluded->contains($relationsShip)) {
-                unset($relations[$key]);
-            }
-        }
-        $query->with($relations);
-    }
-    public function scopeFilter(Builder $query)
-    {
-        if (empty($this->allowFilter) || empty(request('filter'))) {
-            return;
-        }
-        $filters = request('filter');
-        $allowFilter = collect($this->allowFilter);
-        foreach ($filters as $filter => $value) {
-            if ($allowFilter->contains($filter)) {
-                $query->where($filter, 'LIKE', '%' . $value . '%');
-            }
-        }
-    }
-    public function scopeSort(Builder $query)
-    {
-        if (empty($this->allowSort) || empty(request('sort'))) {
-            return;
-        }
-        $sortFields = explode(',', request('sort'));
-        $allowSort = collect($this->allowSort);
-        foreach ($sortFields as $sortField) {
-            $direction = 'asc';
-            if (substr($sortField, 0, 1) == '-') {
-                $direction = 'desc';
-                $sortField = substr($sortField, 1);
-            }
-            if ($allowSort->contains($sortField)) {
-                $query->orderBy($sortField, $direction);
-            }
-        }
-    }
+    
 }
